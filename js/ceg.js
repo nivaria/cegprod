a0da25f1df66a6a7bd127e9e6e61e52261619786
dbrouard <diego@net-monsters.com>
2013-02-14 13:41:53 +0000

    initial commit

---
+++ js/ceg.js
@@ -0,0 +1,14 @@
+jQuery(document).ready(function($){
+    jQuery(".imagecache-active_users_faces").each( function(){
+       jQuery(this).attr("src","http://ceg/sites/default/files/imagecache/profile_pictures/default-user.png"); 
+    });
+    /* TODO: use nth-child pseudoselectors */
+    jQuery("#nav-group ul.menu li").each( function(index){
+        if(index==1){
+            jQuery(this).addClass("second");
+        }
+        if(index==2){
+            jQuery(this).addClass("third");
+        }
+    });
+});
