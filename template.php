398e1a9bcf84a982c6fd56c42ef1688d25a55825
hdeleon <hdeleon@nivaria.com>
2013-03-04 14:01:24 +0000

    Update template.php
    PCEG-148 # función que separa los términos por taxonomía

--- template.php
+++ template.php
@@ -216,3 +216,29 @@ function cegprod_date_combo($element) {
   );
   return theme('fieldset', $fieldset);
 }
+
+/**
+ * Theme to separate terms of taxonomy
+ * @param type $node_taxonomy
+ * @return type 
+ */
+function cegprod_separate_terms($node_taxonomy) {
+  if ($node_taxonomy) { 
+  //separating terms by vocabularies 
+    foreach ($node_taxonomy AS $term) { 
+      $links[$term->vid]['taxonomy_term_'. $term->tid] = array(
+        'title' => $term->name,
+        'href' => taxonomy_term_path($term),
+        'attributes' => array(
+          'rel' => 'tag', 
+          'title' => strip_tags($term->description)
+          ),
+       ); 
+   } //theming terms out 
+ 
+    foreach ($links AS $key => $vid) {
+      $terms[$key] = theme_links($vid); 
+    } 
+  } 
+  return $terms; 
+}
