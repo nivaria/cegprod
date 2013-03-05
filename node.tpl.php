c7189443c030a41956a8c1fc67f4645957c60e8a
hdeleon <hdeleon@nivaria.com>
2013-03-05 16:05:57 +0000

    Update node.tpl.php
    PCEG-148 # Separamos las etiquetas de las Áreas de Gestión

--- node.tpl.php
+++ node.tpl.php
@@ -1,5 +1,7 @@
 <?php
 // $Id: node.tpl.php 7510 2010-06-15 19:09:36Z sheena $
+  $terms = cegprod_not_include_terms($node->taxonomy, '5');
+  $area_terms = cegprod_separate_terms($node->taxonomy);
 ?>
 
 <div id="node-<?php print $node->nid; ?>" class="node <?php print $node_classes; ?>">
@@ -28,6 +30,11 @@
     <?php if ($terms): ?>
     <div class="terms">
       <?php print $terms; ?>
+    </div>
+    <?php endif;?>
+    <?php if ($area_terms[5]): ?>
+    <div class="area_terms">
+      Area de Gestión:<?php print $area_terms[5]; ?>
     </div>
     <?php endif;?>
       <?php print $content ?>
