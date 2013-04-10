9682f7a47e2ed79f7da5f1952056523c4b01b387
dbrouard <diego@net-monsters.com>
2013-04-10 12:36:59 +0100

    detalle de reto: default image if does not exist

--- js/ceg.js
+++ js/ceg.js
@@ -37,4 +37,13 @@ jQuery(document).ready(function($){
             jQuery("body.node-type-idea .field-field-valid-date")
         )
     )
+    if( jQuery("body.node-type-idea .submitted a img").length==0 || jQuery("body.node-type-challenge .submitted a img").length==0 ){
+        jQuery("body .submitted a").each( function(){
+            if( jQuery(this).parent(".username").length==0 ){
+                jQuery(this).append(
+                    jQuery( "<img width='40' height='40' class='imagecache imagecache-users_40_40_ceg' src='/system/files/imagecache/users_cabecera_ceg/default-user.png' />" )
+                )
+            }
+        });
+    }
 });
