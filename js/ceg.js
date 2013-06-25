9c948b90b8f09e4c32c9304aa49a580ae3595c2b
dbrouard <diego@net-monsters.com>
2013-06-25 15:53:07 +0100

    https://nivaria.jira.com/browse/PCEG-333

--- js/ceg.js
+++ js/ceg.js
@@ -63,8 +63,8 @@ jQuery(document).ready(function($){
         jQuery("<h3 class='view-challenge-ideas-title' >Ideas propuestas</h3>"),
         jQuery(".view-challenge-ideas")
     );
-    jQuery("body.node-type-question #content-content").after(
-        jQuery("body.node-type-question #content-content .inner .content h2").addClass("view-question-answers-title"),
+    jQuery("body.node-type-question #content-content").after(        
+        jQuery("<h3 class='view-question-answers-title' >"+jQuery("h1").text()+"</h3>"),
         jQuery(".view-question-answers")
     );
 });
